d2ab4e87d4143df51bbe365a7f5506dc56640493
Ilya Choroszun <60191028+Mr-Cronk@users.noreply.github.com>
2022-03-22 20:52:06 +0100

    Adding a database.

--- rest-api-smart/app.py
+++ rest-api-smart/app.py
@@ -1,8 +1,23 @@
 from flask import Flask, jsonify, request
+import sqlalchemy as db
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 
 app = Flask(__name__)
 
-#client = app.test_client()
+client = app.test_client()
+
+engine = create_engine('sqlite:///db.sqlite')
+
+session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
+Base = declarative_base()
+Base.query = session.query_property()
+
+from models import *
+
+Base.metadata.create_all(bind=engine)
 
 shops = [
     {
@@ -25,32 +40,61 @@ shops = [
 
 @app.route('/shops', methods=['GET'])
 def get_list():
-    return jsonify(shops)
+    stores = Store.query.all()
+    serialised = []
+    for store in stores:
+        serialised.append({
+            'id': store.id,
+            'title': store.title,
+            'description': store.description
+        })
+    return jsonify(serialised)
 
 
 @app.route('/shops', methods=['POST'])
 def update_list():
-    new_one = request.json
-    shops.append(new_one)
-    return jsonify(shops)
+    new_one = Store(**request.json)
+    session.add(new_one)
+    session.commit()
+    serialised = {
+        'id': new_one.id,
+        'title': new_one.title,
+        'description': new_one.description
+    }
+    return jsonify(serialised)
 
 
 @app.route('/shops/<int:shop_id>', methods=['PUT'])
 def update_shop(shop_id):
-    item = next((x for x in shops if x['id'] == shop_id), None)
+    item = Store.query.filter(Store.id == shop_id).first()
     params = request.json
     if not item:
         return {'message': 'No shop with the same id'}, 400
-    item.update(params)
-    return item
+    for key, value in params.items():
+        setattr(item, key, value)
+    session.commit()
+    serialised = {
+        'id': item.id,
+        'title': item.title,
+        'description': item.description
+    }
+    return serialised
 
 
 @app.route('/shops/<int:shop_id>', methods=['DELETE'])
 def delete_shop(shop_id):
-    idx, _ = next((x for x in enumerate(shops) if x[1]['id'] == shop_id), (None, None))
-
-    shops.pop(idx)
+    item = Store.query.filter(Store.id == shop_id).first()
+    if not item:
+        return {'message': 'No shop with the same id'}, 400
+    session.delete(item)
+    session.commite()
     return '', 204
+
+
+@app.teardown_appcontext
+def shutdown_session(exeption=None):
+    session.remove()
+
 
 
 if __name__ == '__main__':
