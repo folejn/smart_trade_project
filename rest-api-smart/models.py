a012381dca70e9ec00522d1110af36c71f973eda
Filip Olejniczak <folejn@gmail.com>
2022-05-03 17:36:22 +0200

    models: orm relationships fixed

--- rest-api-smart/models.py
+++ rest-api-smart/models.py
@@ -1,6 +1,10 @@
 from app import db, session, Base
+from sqlalchemy.orm import relationship
 
-
+transaction_user = db.Table('junction', Base.metadata,
+    db.Column('users_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('transaction_id', db.Integer, db.ForeignKey('transaction.id'))
+)
 class Store(Base):
     __tablename__ = 'stores'
     id = db.Column(db.Integer, primary_key=True)
@@ -14,6 +18,7 @@ class User(Base):
     name = db.Column(db.String(30), nullable=False)
     surname = db.Column(db.String(30), nullable=False)
     birth_date = db.Column(db.DateTime, nullable=False)
+    products = relationship('Product', backref='owner', lazy=True)
 
 
 class Product(Base):
@@ -31,3 +36,5 @@ class Transaction(Base):
     date = db.Column(db.DateTime, nullable=False)
     quantity = db.Column(db.Integer, nullable=True)
     price = db.Column(db.Integer, nullable=True)
+    sides = relationship("User",
+                    secondary=transaction_user, backref='transactions')
