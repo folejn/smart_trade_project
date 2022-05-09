98b9b7b4eca0789da9ffa5561c8503b4b0a52334
Ilya Choroszun <60191028+Mr-Cronk@users.noreply.github.com>
2022-05-09 19:12:02 +0200

    Update app.py

--- rest-api-smart/app.py
+++ rest-api-smart/app.py
@@ -4,17 +4,14 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from flask_jwt_extended import JWTManager
-from config import Config
 
 app = Flask(__name__)
-app.config.from_object(Config)
 
 client = app.test_client()
 
 engine = create_engine('sqlite:///db.sqlite')
 
-session = scoped_session(sessionmaker(
-    autocommit=False, autoflush=False, bind=engine))
+session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base = declarative_base()
 Base.query = session.query_property()
@@ -105,6 +102,7 @@ def get_products():
             'id': product.id,
             'name': product.name,
             'price': product.price,
+            'description': product.description,
             'status': product.status,
             'quantity': product.quantity
         })
@@ -153,7 +151,7 @@ def update_stores():
 
 
 @app.route('/products', methods=['POST'])
-def update_products():
+def new_product():
     new_one = Product(**request.json)
     session.add(new_one)
     session.commit()
@@ -182,7 +180,7 @@ def update_users():
 
 
 @app.route('/transaction', methods=['POST'])
-def update_transaction():
+def new_transaction():
     new_one = Transaction(**request.json)
     session.add(new_one)
     session.commit()
@@ -230,6 +228,38 @@ def update_product(product_id):
     }
     return serialised
 
+@app.route('/products/name/<string:product_subname>', methods=['GET'])
+def search_product_by_name(product_subname):
+    products = Product.query.filter(Product.name.contains(product_subname)).all()
+    if not products:
+        return jsonify([])
+    serialised = []
+    for product in products:
+        serialised.append({
+            'id': product.id,
+            'name': product.name,
+            'description': product.description,
+            'quantity': product.quantity,
+            'price': product.price
+        })
+    return jsonify(serialised)
+
+@app.route('/products/description/<string:product_subdescr>', methods=['GET'])
+def search_product_by_desc(product_subdescr):
+    products = Product.query.filter(Product.description.contains(product_subdescr)).all()
+    if not products:
+        return jsonify([])
+    serialised = []
+    for product in products:
+        serialised.append({
+            'id': product.id,
+            'name': product.name,
+            'description': product.description,
+            'quantity': product.quantity,
+            'price': product.price
+        })
+    return jsonify(serialised)
+
 
 @app.route('/transaction/<int:transaction_id>', methods=['PUT'])
 def update_transaction(transaction_id):
@@ -259,13 +289,23 @@ def delete_shop(shop_id):
     return '', 204
 
 
+@app.route('/register', methods=['POST'])
+def register():
+    params = request.json
+    user = User(**params)
+    session.add(user)
+    session.commit()
+    token = user.get_token()
+    return {'access_token': token}
+
+
 @app.route('/products/<int:product_id>', methods=['DELETE'])
 def delete_product(product_id):
     item = Product.query.filter(Product.id == product_id).first()
     if not item:
         return {'message': 'No product with the same id'}, 400
     session.delete(item)
-    session.commit()
+    session.commite()
     return '', 204
 
 
@@ -275,18 +315,8 @@ def delete_transaction(transaction_id):
     if not item:
         return {'message': 'No transaction with the same id'}, 400
     session.delete(item)
-    session.commit()
+    session.commite()
     return '', 204
-
-
-@app.route('/register', methods=['POST'])
-def register():
-    params = request.json
-    user = User(**params)
-    session.add(user)
-    session.commit()
-    token = user.get_token()
-    return {'access_token': token}
 
 
 @app.teardown_appcontext
